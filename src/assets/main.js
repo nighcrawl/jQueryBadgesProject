7e23440d08f3c213fa25400a742865b6df77f72a
nighcrawl <ange.chierchia@gmail.com>
2016-12-10 13:54:35 +0100

    affiche la liste des badges gagnés

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,14 @@
 $(function() {
 
   // your code will go here
-
+  jQuery.ajax({
+    url: "https://www.codeschool.com/users/nighcrawl.json",
+    dataType: "jsonp",
+    success: function(response) {
+      console.log(response.courses.completed);
+      jQuery.each(response.courses.completed, function(i, course) {
+        jQuery("#badges").append("<div class='course'><h3>" + course.title + "</h3><img src='" + course.badge + "' title='" + course.title + "' /><a href='" + course.url + "' target='_blank' class='btn btn-primary'>See Course</a></div>");
+      });
+    }
+  });
 });
