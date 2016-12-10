d94e77e81a0d7664c85d623620b27df7fb810448
nighcrawl <ange.chierchia@gmail.com>
2016-12-10 13:59:08 +0100

    supprime le console.log

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,13 +1,12 @@
 $(function() {
 
   // your code will go here
-  jQuery.ajax({
+  $.ajax({
     url: "https://www.codeschool.com/users/nighcrawl.json",
     dataType: "jsonp",
     success: function(response) {
-      console.log(response.courses.completed);
-      jQuery.each(response.courses.completed, function(i, course) {
-        jQuery("#badges").append("<div class='course'><h3>" + course.title + "</h3><img src='" + course.badge + "' title='" + course.title + "' /><a href='" + course.url + "' target='_blank' class='btn btn-primary'>See Course</a></div>");
+      $.each(response.courses.completed, function(i, course) {
+        $("#badges").append("<div class='course'><h3>" + course.title + "</h3><img src='" + course.badge + "' title='" + course.title + "' /><a href='" + course.url + "' target='_blank' class='btn btn-primary'>See Course</a></div>");
       });
     }
   });
